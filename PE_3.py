b1b88d8163f230ebf830551b73387f24605681b8
christina <82810050+ctmazilu@users.noreply.github.com>
2024-05-29 09:21:43 -0400

    PE_3 editt

--- PE_3.py
+++ PE_3.py
@@ -1,3 +1,9 @@
+# Written 02/21/2021 by Christina Mazilu
+# Project Euler Question 3: https://projecteuler.net/problem=3
+
+# The prime factors of 13195 are 5, 7, 13 and 29
+# What is the largest prime factor of the number 600851475143
+
 def pf(number):
     primeFactor = []
     for i in range(2, number):
